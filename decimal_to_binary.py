48e1023d929c265a072c7e492c24c03d6161df90
Jonathan Ramirez <jonathan.ramirez1@snhu.edu>
2025-01-31 09:25:27 -0600

    Edited half bottom portion of code (starting at second function) to comply with Python coding standards

--- decimal_to_binary.py
+++ decimal_to_binary.py
@@ -12,17 +12,21 @@ def decimal_to_binary(number):
         #Divide the number and then round down until it reaches zero
         rounded_number = math.floor(num / 2)
         num = rounded_number
-        
     return convert_list(binary_num) #Calls the convert_list function to flip the list
 
 def convert_list(binary_list):
-    flip_binary_number = binary_list[::-1 ]#Flips the list since binary numbers are written from right to left.
-    convert_to_string = [str(bit) for bit in flip_binary_number] # the list of integers are then converted to strings
-    output_binary = ''
-    for string_bit in convert_to_string:
-        output_binary += string_bit #The reason why we converted the list to strings
-    return output_binary #returns a cleaner output
+
+    # Flips the list since binary numbers are read from right to left
+    flip_binary_number = binary_list[::-1 ]
+    # The list of integers are then converted to strings
+    convert_to_string = [str(bit) for bit in flip_binary_number]
     
+    output_binary = ''
+    for str_bit in convert_to_string:
+        output_binary += str_bit
+    # Returns a cleaner output
+    return output_binary
+
 number_to_binary = int(input('Enter a number to convert to binary: '))
 conversion = decimal_to_binary(number_to_binary)
 print(f'\n{number_to_binary} in binary is: 0b{conversion}')
